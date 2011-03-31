d60c21eb3db5cda3c6970515822181ca2b8b21f2
shinderuman <shinderuman@gmail.com>
2011-04-01 02:20:31 +0900

    remove old files

--- models/SeqDataModel.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#encoding:utf-8
-__author__ = 'lunch@spookies.in'
-
-from google.appengine.ext import db
-
-class SeqDataModel(db.Model):
-  mid = db.IntegerProperty()
-  cnt = db.IntegerProperty()
